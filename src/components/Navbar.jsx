8cf0bdef529d31f7bdba300ef3bd3908e6085275
marufur-rahman <mdmarufur5682@gmail.com>
2023-04-25 07:30:18 +0600

    home page done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import { NavLink } from "react-router-dom";
 
-const Navbar = ({ setSearchQuery, searchQuery, inputField, searchHandler }) => {
+const Navbar = ({
+	searchHandler,
+	searchQuery,
+	setSearchQuery,
+	inputField,
+	savedItems,
+}) => {
 	const navActive = ({ isActive }) => {
 		return {
 			color: isActive ? "#f43f5e" : null,
@@ -10,27 +16,26 @@ const Navbar = ({ setSearchQuery, searchQuery, inputField, searchHandler }) => {
 	return (
 		<div className="navbar flex justify-between items-center container mx-auto py-8 flex-col lg:flex-row gap-5 lg:gap-0">
 			<h2 className="logo text-2xl font-bold lowercase italic">
-				Food<span className="text-rose-500">maker</span>
+				Food<span className="text-rose-500">verse</span>
 			</h2>
 			<form className="search-bar" onSubmit={searchHandler}>
 				<input
-					name="search"
 					ref={inputField}
-					// value={searchQuery}
-					// onChange={(e) => setSearchQuery(e.target.value)}
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
 					type="search"
 					placeholder="Search recipe..."
 					required
 					className="bg-white/75 p-3 px-8 lg:w-96 rounded-full outline-none shadow-lg shadow-rose-100 focus:shadow-rose-200 duration-300"
 				/>
 			</form>
-			<ul className="menu flex flex-col gap-4 lg:flex-row">
+			<ul className="menu flex gap-5">
 				<li>
 					<NavLink
 						style={navActive}
 						end
 						to="/"
-						className="text-gray-400 hover:text-gray-600">
+						className="text-gray-400 hover:text-gray-600 duration-300">
 						Home
 					</NavLink>
 				</li>
@@ -38,10 +43,10 @@ const Navbar = ({ setSearchQuery, searchQuery, inputField, searchHandler }) => {
 					<NavLink
 						style={navActive}
 						to="/favourites"
-						className="text-gray-400 hover:text-gray-600">
+						className="text-gray-400 hover:text-gray-600 duration-300">
 						Favourites{" "}
 						<span className="favourites-count font-bold text-sky-400">
-							{10}
+							({savedItems.length})
 						</span>
 					</NavLink>
 				</li>
